8b16fb604bf9091a75720b2695a88c014ce576b3
hendhd <heinl@ari.uni-heidelberg.de>
2020-11-08 06:42:10 +0100

    Corrected neutrino.vot, tidied query

--- pysrc/example4/example4.py
+++ pysrc/example4/example4.py
@@ -6,16 +6,14 @@
 import pyvo
 from astropy.table import Table
 
-
-
 QUERY="""
 SELECT
-   TOP 10
+   TOP 50
    *
    FROM ivoa.obscore AS db
-   JOIN TAP_UPLOAD.lt AS tc
+   JOIN TAP_UPLOAD.lt AS mine
    ON 1=CONTAINS(POINT('ICRS', db.s_ra, db.s_dec),
-                 CIRCLE('ICRS', tc.RA, tc.Decl, Beta))
+                 CIRCLE('ICRS', mine.RA, mine.Decl, mine.Beta))
 """
 
 def search_obscsvc(services, lt):
@@ -39,33 +37,18 @@ def search_obscsvc(services, lt):
      print("  Broken: {} ({})\n".format(row.access_url, msg))
 
 
-def search_ssvc(svcurl, lt):
-  try:
-    service=pyvo.dal.TAPService(svcurl)
-    result=service.run_async(query=QUERY, uploads={"lt":lt})
-    result.broadcast_samp("topcat")
-  except KeyboardInterrupt:
-      # someone lost their patience with a service.  Query next.
-    pass
-  except Exception as msg:
-    # some service is broken; you *should* complain, but
-    # then let's be lazy here
-    print("  Broken: {} ({})\n".format(svcurl, msg))
-
 
 def main():
   # get local neutrino data
-  lt = Table.read('neutrinos.xml', format='votable')
+  lt = Table.read('../example2/neutrinos.vot', format='votable')
   print (lt)
 
   # Search the registry and save the result
 
   obsvc=pyvo.registry.search(datamodel="obscore")
-  obsvc.votable.to_xml("obscore_services.xml")
+  obsvc.votable.to_xml("obscore_services.vot")
 
   search_obscsvc(obsvc, lt)
-#  search_ssvc("http://dc.zah.uni-heidelberg.de/tap", lt)
-
 	
 if __name__=="__main__":
     main()
